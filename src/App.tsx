4163810cfcf38f2db3ad6830e771b8e992bcb042
zznixt07 <zznixt07@protonmail.com>
2022-01-11 18:52:02 +0545

    typed custom hooks

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ const App = (): JSX.Element => {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Yo</h1>
       </header>
     </div>
   );
--- src/CustomHooks.tsx
+++ src/CustomHooks.tsx
@@ -1,31 +1,34 @@
 import React from 'react'
 
+type voidFn = () => void
+
 // sauce: https://www.joshwcomeau.com/snippets/react-hooks/use-timeout/
-const useTimeout = (callback, delay) => {
-    const timeoutRef = React.useRef(null)
-    const savedCallback = React.useRef(callback)
+const useTimeout = (callback: voidFn, delay: number | null): React.MutableRefObject<number | undefined> => {
+    const timeoutRef = React.useRef<number | undefined>(undefined)
+    const savedCallback = React.useRef<voidFn>(callback)
     React.useEffect(() => {
         savedCallback.current = callback
     }, [callback])
     React.useEffect(() => {
-        const tick = () => savedCallback.current()
+        const tick: voidFn = () => savedCallback.current()
         if (typeof delay === 'number') {
             timeoutRef.current = window.setTimeout(tick, delay)
             return () => window.clearTimeout(timeoutRef.current)
         }
+       
     }, [delay])
     return timeoutRef
 }
 
 // sauce: https://www.joshwcomeau.com/snippets/react-hooks/use-interval/
-const useInterval = (callback, delay) => {
-    const intervalRef = React.useRef(null)
-    const savedCallback = React.useRef(callback)
+const useInterval = (callback: voidFn, delay: number | null): React.MutableRefObject<number | undefined> => {
+    const intervalRef = React.useRef<number | undefined>(undefined)
+    const savedCallback = React.useRef<voidFn>(callback)
     React.useEffect(() => {
         savedCallback.current = callback
     }, [callback])
     React.useEffect(() => {
-        const tick = () => savedCallback.current()
+        const tick: voidFn = () => savedCallback.current()
         if (typeof delay === 'number') {
             intervalRef.current = window.setInterval(tick, delay)
             return () => window.clearInterval(intervalRef.current)
