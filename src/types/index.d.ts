eaae3282803ff4bb981e37e94d43990eb9a026c1
zznixt07 <zznixt07@protonmail.com>
2022-01-15 00:08:41 +0545

    added d.ts for images and svgs

---
+++ src/types/index.d.ts
@@ -0,0 +1,11 @@
+declare module '*.jpg' {
+    const src: string
+    export default src
+}
+
+declare module '*.svg' {
+    import * as React from 'react';
+    export const ReactComponent: React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+    const src: string;
+    export default src;
+}
