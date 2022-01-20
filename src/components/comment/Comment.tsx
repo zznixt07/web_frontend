f4fd4dcea67fab8d0b2f031fe5605785ea9f38bd
zznixt07 <zznixt07@protonmail.com>
2022-01-20 07:41:19 +0545

    add comment component

---
+++ src/components/comment/Comment.tsx
@@ -0,0 +1,285 @@
+import * as React from 'react'
+import styled, { css } from 'styled-components'
+import { CommentForm } from './CommentForm'
+
+const FlexWrapAlign = styled.div`
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    gap: 0.5rem;
+`
+
+const GeneralCommentContainer = styled.div`
+    border: 1px solid #eae6e6;
+    padding: 1rem 0.8rem;
+    border-radius: 0.4rem;
+    margin: 0.3rem 0;
+`
+
+const CommentContainer = styled(GeneralCommentContainer)<{indent: number}>`
+    margin-left: ${(props) => props.indent + 'rem'};
+`
+
+const CommentHead = styled(FlexWrapAlign)`
+    justify-content: space-between;
+`
+
+const Author = styled(FlexWrapAlign)`
+    gap: 0.4rem;
+    flex: 1 1 0;
+    & > ::before {
+        content: '· ';
+    }
+    span:first-of-type::before {
+        content: '';
+    }
+`
+
+const Options = styled.div``
+
+const AuthorImage = styled.img`
+    width: 2rem;
+    height: 2rem;
+    object-fit: cover;
+    border-radius: 50%;
+`
+
+const CommentText = styled.div`
+    margin: 0.4rem 0.2rem 0.6rem 0.2rem;
+`
+
+const CommentBottomAction = styled(FlexWrapAlign)`
+    justify-content: space-between;
+    margin: 0.3rem;
+`
+
+const CommentAction = styled(FlexWrapAlign)`
+    gap: 1rem;
+`
+
+const Reaction = styled(FlexWrapAlign)`
+    position: relative;
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+`
+
+const ReactionList = styled(FlexWrapAlign)<{reacted?: boolean}>`
+    gap: 0.2rem;
+    cursor: pointer;
+    padding: 0.1rem 0.4rem;
+    border: 1px solid black;
+    border-radius: 1rem;
+    &::selection,
+    & *::selection {
+        background: #ffffff00;
+    }
+    &::-moz-selection,
+    & *::-moz-selection {
+        background: #ffffff00;
+    }
+    &:hover {
+        background-color: #6c9ae227;
+    }
+    ${(props) =>
+        props.reacted &&
+        css`
+            background-color: #6c9ae247;
+            color: blue;
+        `}
+`
+
+type ReactionItemProp = {
+    emoji: string
+    count: number
+    reacted: boolean
+    onClick: any
+}
+
+const ReactionItem = ({ emoji, count, reacted, onClick }: ReactionItemProp) => {
+    return (
+        <ReactionList
+            as='li'
+            reacted={reacted}
+            onClick={onClick}
+            data-id={emoji}
+        >
+            <div>{emoji}</div>
+            <div>{count}</div>
+        </ReactionList>
+    )
+}
+
+const EmojiChooser = styled.div`
+    display: none;
+    font-size: 1.2rem;
+    position: absolute;
+    top: -200%;
+    left: -5px;
+    gap: 0.2rem;
+    outline: 2px solid #c47d1e;
+    border-radius: 0.6rem;
+    padding: 0.2rem;
+    z-index: 1;
+`
+
+const AddEmoji = styled.span`
+    position: relative;
+    cursor: pointer;
+    &:hover > ${EmojiChooser} {
+        display: flex;
+    }
+`
+
+const PopupEmoji = styled.span`
+    padding: 0.2rem;
+    border-radius: 1rem;
+    &:hover {
+        background-color: #d8dfea;
+        outline: 1px solid;
+        cursor: pointer;
+    }
+`
+
+type CommentProp = {
+    id: string
+    reactionUpdateEndpoint: string
+    by: string
+    profileImg: string
+    text: string
+    time: string
+    edited: boolean
+    indent: number
+    replyIdSetter: any
+    reactionsArr: any[]
+    onReactAsync: any
+}
+
+const Comment = ({
+    id,
+    reactionUpdateEndpoint,
+    by,
+    profileImg,
+    text,
+    time,
+    edited,
+    indent,
+    replyIdSetter,
+    reactionsArr,
+    onReactAsync,
+}: CommentProp) => {
+    const [reactions, setReactions] = React.useState(reactionsArr)
+    const [reactionUpdateUrl, setReactionUpdateUrl] = React.useState(reactionUpdateEndpoint)
+
+    const reactionClicked = async (reaction: any) => {
+        const recs = []
+        for (const r of reactions) {
+            if (r.id === reaction.id) {
+                // setLoadingIndicator(reactionId, true)
+                const reactionRemoved = !!r.reacted
+
+                const [data, createdUrl] = await onReactAsync(r.id, reactionRemoved)
+                if (!reactionUpdateUrl) {
+                    // if the POST succeeded, then we now know the PATCH url.
+                    // if failed, will be null which is what we want.
+                    setReactionUpdateUrl(createdUrl)
+                }
+
+                // setLoadingIndicator(reactionId, false)
+                if (data.error) {
+                    // showToast('failed')
+                } else {
+                    if (reactionRemoved) {
+                        r.count--
+                    } else {
+                        r.count++
+                    }
+                    r.reacted = !r.reacted
+                }
+            }
+            recs.push(r)
+        }
+        setReactions(recs)
+    }
+
+    const AddReaction = ({ hiddenReactions }: any) => {
+        // console.log({ hiddenReactions })
+        return (
+            <>
+                <AddEmoji>
+                    ✨
+                    <EmojiChooser>
+                        {hiddenReactions.map((r: any) => (
+                            <PopupEmoji
+                                key={r.id}
+                                onClick={async () => await reactionClicked(r)}
+                            >
+                                {r.id}
+                            </PopupEmoji>
+                        ))}
+                    </EmojiChooser>
+                </AddEmoji>
+            </>
+        )
+    }
+    const unusedReactions = reactions.filter((r) => r.count === 0)
+    return (
+        <CommentContainer indent={indent} data-id={id} id={id}>
+            <CommentHead>
+                <Author>
+                    <AuthorImage src={profileImg} />
+                    <span>{by}</span>
+                    <span className='date'>
+                        {new Date(time).toLocaleString()}
+                    </span>
+                    {edited ? (
+                        <span>
+                            <i>{'edited'}</i>
+                        </span>
+                    ) : null}
+                </Author>
+                <Options>
+                    <button>...</button>
+                </Options>
+            </CommentHead>
+            <CommentText>{text}</CommentText>
+            <CommentBottomAction>
+                <CommentAction>
+                    <Reaction as='ul'>
+                        {reactions.map((reaction) => {
+                            return reaction.count ? (
+                                <ReactionItem
+                                    key={reaction.id}
+                                    emoji={reaction.id}
+                                    count={reaction.count}
+                                    reacted={reaction.reacted}
+                                    onClick={async () =>
+                                        await reactionClicked(reaction)
+                                    }
+                                />
+                            ) : null
+                        })}
+                        <li>
+                            {unusedReactions.length > 0 ? (
+                                <AddReaction
+                                    hiddenReactions={unusedReactions}
+                                />
+                            ) : null}
+                        </li>
+                    </Reaction>
+                    <div className='reply'>
+                        <button
+                            // variant='outlined'
+                            onClick={() => replyIdSetter(id)}
+                        >
+                            Reply
+                        </button>
+                    </div>
+                </CommentAction>
+                <div className='expand-collapse'>Collapse</div>
+            </CommentBottomAction>
+        </CommentContainer>
+    )
+}
+
+export default Comment
