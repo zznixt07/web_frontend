604037f562f9cc8d03303bf724bc06ba7a09d84f
zznixt07 <zznixt07@protonmail.com>
2022-01-22 14:25:28 +0545

    added video action icons

---
+++ src/assets/svg/FlagOutlined.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function FlagOutlined(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M14 6l-1-2H5v17h2v-7h5l1 2h7V6h-6zm4 8h-4l-1-2H7V6h5l1 2h5v6z" fill="currentColor"></path></svg>
+  )
+}
+export default FlagOutlined
---
+++ src/assets/svg/Share.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function Share(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><path d="M21 12l-7-7v4C7 10 4 15 3 20c2.5-3.5 6-5.1 11-5.1V19l7-7z" fill="currentColor"></path></svg>
+  )
+}
+export default Share
