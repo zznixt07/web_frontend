8be3472974a8f95c5384a34bf5669ccf1ff4f480
zznixt07 <zznixt07@protonmail.com>
2022-01-25 14:12:29 +0545

    icon files

---
+++ src/assets/svg/AkarIconsLinkChain.tsx
@@ -0,0 +1,8 @@
+import React, { SVGProps } from 'react'
+
+export function AkarIconsLinkChain(props: SVGProps<SVGSVGElement>) {
+  return (
+    <svg width="1em" height="1em" viewBox="0 0 24 24" {...props}><g fill="none"><path d="M13.544 10.456a4.368 4.368 0 0 0-6.176 0l-3.089 3.088a4.367 4.367 0 1 0 6.177 6.177L12 18.177" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path><path d="M10.456 13.544a4.368 4.368 0 0 0 6.176 0l3.089-3.088a4.367 4.367 0 1 0-6.177-6.177L12 5.823" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"></path></g></svg>
+  )
+}
+export default AkarIconsLinkChain
