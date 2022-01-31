ae0d731349f59e8d179a9a5e658fcc5a7d4dce7d
zznixt <34276575+zznixt07@users.noreply.github.com>
2022-01-31 00:14:35 +0000

    trying thumnail using VTT file

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -9,22 +9,39 @@ type PlayerProps = {
     // description: string;
 };
 
-const Player = ({src}: PlayerProps) => {
+const OPTIONS = {
+  controls: [
+    "play",
+    "play-large",
+    "progress",
+    "settings",
+    "current-time",
+    "fullscreen",
+  ],
+  // settings: ["captions", "quality", "loop"],
+  disableContextMenu: true,
+  resetOnEnd: true,
+  tooltips: { controls: true, seek: true },
+  quality: { default: 720, options: [1440, 1080, 720, 480, 360] },
+  ratio: "16:9",
+  previewThumbnails: {
+    enabled: true,
+    src: "https://portal-everywhere.herokuapp.com/proxy/https://zznixt07-web-backend-rx4p4wjvf59x7-8000.githubpreview.dev/static/720p.vtt",
+  },
+};
+
+const Player = ({ src }: PlayerProps) => {
   return (
     <Plyr
       source={{
         type: "video",
         title: "Title",
-        sources: [{ src: src, type: "video/mp4", size: 720 }],
+        sources: [
+          { src: src, type: "video/mp4", size: 720 },
+          { src: src, type: "video/mp4", size: 480 },
+        ],
       }}
-      options={{
-        controls: ["play", "progress", "settings", "fullscreen"],
-        settings: ["captions", "quality", "loop"],
-        disableContextMenu: true,
-        resetOnEnd: true,
-        tooltips: { controls: true, seek: true },
-        quality: { default: 720, options: [1440, 1080, 720, 480, 360] },
-      }}
+      options={OPTIONS}
     />
   );
 };
